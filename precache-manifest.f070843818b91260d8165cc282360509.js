a5a37f5a104baa6f5b3a2c0505c108f3e534870d
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2020-06-27 21:15:44 +0800

    Updates

---
+++ precache-manifest.f070843818b91260d8165cc282360509.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4aad2e8f811b055f9d74abf58879ed",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/index.html"
+  },
+  {
+    "revision": "d192b3790c42468b29a4",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/css/main.e424f81e.chunk.css"
+  },
+  {
+    "revision": "5e8268998e946b522952",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/js/2.91fd39ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/js/2.91fd39ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d192b3790c42468b29a4",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/js/main.69c223f0.chunk.js"
+  },
+  {
+    "revision": "d276f517e671e6217b11",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/js/runtime-main.9c0df982.js"
+  },
+  {
+    "revision": "191963967d12bb1d9e821912f546a22c",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/media/LOGO.19196396.svg"
+  },
+  {
+    "revision": "7d4a4511e2e44d628ced928514c23c86",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/media/beans.7d4a4511.jpg"
+  },
+  {
+    "revision": "13aee71b80180d00faca1b7b987a0d4b",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/media/coffeecup.13aee71b.jpg"
+  },
+  {
+    "revision": "0e58fcfeaa73af8bcc76cadb7605b3da",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/media/farmer.0e58fcfe.jpg"
+  },
+  {
+    "revision": "2979cd57c24c319c19bb714d70652848",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/media/front.2979cd57.jpg"
+  },
+  {
+    "revision": "281e4dbdafd29473d9896ccfdfea6fea",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/media/inside.281e4dbd.jpg"
+  },
+  {
+    "revision": "c1e8a7afb9676bc824f0838f5cf36537",
+    "url": "/kitharvey/kitharvey.github.io/cockcrow/static/media/sunrise.c1e8a7af.jpg"
+  }
+]);
